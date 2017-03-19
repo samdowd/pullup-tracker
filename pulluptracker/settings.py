01aff59d9bf413f8bd7c6167aa518930813aa90a
Sam Dowd <dowd95@gmail.com>
2017-03-19 07:20:38 +0000

    a

--- pulluptracker/settings.py
+++ pulluptracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '))ra_tdx7_=@t0=03zjx+-5m8vq+kaa94$(gi4s!(5m_+^tke7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
